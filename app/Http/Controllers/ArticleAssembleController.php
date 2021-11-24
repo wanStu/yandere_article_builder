f2e7c2c9aefef8f6f16fa36cc9db9c546e0ea6d2
wcy <w_c_y_929@163.com>
2021-11-24 15:18:58 +0800

    算法优化

--- app/Http/Controllers/ArticleAssembleController.php
+++ app/Http/Controllers/ArticleAssembleController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Services\Component;
 use App\Http\Services\GetArticleComponent;
 use App\Http\Services\common;
 /**
@@ -24,39 +23,55 @@ class ArticleAssembleController
         $name = $this->requestParam["SVO"];
         $article = "";
         $articleMmodule = $this->getArticleComponent->getAssemble();
-        $articleMmodule["personal_pronoun"] = [
-            "你","我","他","她","它"
+        $articleMmodule["you"] = [
+            "是","是","你是"
         ];
-        $adjectiveSum = count($articleMmodule["adjectiveList"]);//形容词
-        $modalParticleSum = count($articleMmodule["modalParticleList"]);//语气词
-        $nounSum = count($articleMmodule["nounList"]);//名词
-        $verbSum = count($articleMmodule["verbList"]);//动词
-        $puncSum = count($articleMmodule["puncList"]);//标点
-        $personalPronounSum = count($articleMmodule["personal_pronoun"]);//人称代词
-        for (;strlen($article) <= 1000;) {
-            $adjectiveNum = rand(0,$adjectiveSum - 1);
-            $modalParticleNum = rand(0,$modalParticleSum - 1);
+        $articleMmodule["me"] = [
+            "I" => "我", "my" => "我的"
+        ];
+        $adjectiveSum = count($articleMmodule["adjectiveList"]);//形容词 16
+        $modalParticleSum = count($articleMmodule["modalParticleList"]);//语气词 8
+        $nounSum = count($articleMmodule["nounList"]);//名词 36
+        $verbSum = count($articleMmodule["verbList"]);//动词 36
+        $puncSum = count($articleMmodule["puncList"]);//标点 7
+        $youSum = count($articleMmodule["you"]);//人称代词 3
+        $addNumOne = rand(3,6);                 //添加次数
+        $addNumTwo = rand(3,6);
+        $addNumThree = abs($addNumOne - $addNumTwo)?:1;
+        $nounNum = rand(0,$nounSum - 1);
+        $puncNum = rand(0,$puncSum - 1);
+        $article .= $articleMmodule["modalParticleList"][rand(0,$modalParticleSum - 1)]             //语气词
+            .$articleMmodule["puncList"][rand(0,$puncNum)]
+            .$articleMmodule["me"]["my"].$name                                                 //我的 name(对象名)
+            .$articleMmodule["puncList"][rand(0,$puncNum)]
+            .$articleMmodule["me"]["my"].$articleMmodule["nounList"][rand(0,$nounNum)]."般的".$name //我的 noun(名词) 般的  name(对象名)
+            .$articleMmodule["puncList"][rand(0,$puncNum)];
+        for(;$addNumOne--;) {
             $nounNum = rand(0,$nounSum - 1);
-            $verbNum = rand(0,$verbSum - 1);
             $puncNum = rand(0,$puncSum - 1);
-            $personalPronounNum = rand(0,$personalPronounSum - 1);
-            $article .=
-                $articleMmodule["modalParticleList"][$modalParticleNum]
-                .$name
-                .$articleMmodule["personal_pronoun"][$personalPronounNum]."是"
-                .$articleMmodule["personal_pronoun"][$personalPronounNum]."的"
-                .$articleMmodule["adjectiveList"][$adjectiveNum]
-                .$articleMmodule["puncList"][$puncNum]."是"
-                .$articleMmodule["personal_pronoun"][$personalPronounNum]."的"
-                .$articleMmodule["nounList"][$nounNum]
-                .$articleMmodule["puncList"][$puncNum]
-                .$articleMmodule["personal_pronoun"][$personalPronounNum]
-                .$articleMmodule["puncList"][$puncNum]
-                .$articleMmodule["verbList"][$verbNum]
-                .$articleMmodule["puncList"][$puncNum];
+            $youNum = rand(0,$youSum - 1);
+            $article .= $articleMmodule["you"][$youNum].$articleMmodule["nounList"][rand(0,$nounNum)]; // you（是，你是） noun(名词)
+            $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
+            $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
         }
+         $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
+        for(;$addNumTwo--;) {
+            $youNum = rand(0,$youSum - 1);
+            $puncNum = rand(0,$puncSum - 1);
+            $adjectiveNum = rand(0,$adjectiveSum -1);
+            $article .= $articleMmodule["you"][$youNum];
+            $article .= $articleMmodule["adjectiveList"][rand(0,$adjectiveNum)]; //  adjective(形容词)
+            $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
+        }
+         $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
+        for(;$addNumThree--;) {
+            $puncNum = rand(0,$puncSum - 1);
+            $verbNum = rand(0,$verbSum - 1);
+            $article .= $articleMmodule["me"]["I"].$articleMmodule["verbList"][rand(0,$verbNum)]; //  我 verb(动词)
+            $article .= $articleMmodule["puncList"][rand(0,$puncNum)];
+        }
+         $article .= $articleMmodule["puncList"][rand(0,$puncNum)].$articleMmodule["nounList"][rand(0,$nounNum)];
         return view("article",compact("name","article"));
-//        return $this->common->returnJson(200,"生成完毕",$article);
     }
 
     public function getComponentList() {

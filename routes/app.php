763bd7bf541f97e8f0b6e25846c04c4f71799d82
wcy <w_c_y_929@163.com>
2021-12-01 13:59:19 +0800

    用户相关路由

--- routes/app.php
+++ routes/app.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ArticleAssembleController;
 use App\Http\Controllers\ComponentController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Services\CommonService;
 
@@ -21,16 +22,28 @@ Route::match(["post","get"],'assemble', [ArticleAssembleController::class,"assem
 
 //需要验证 token 的路由
 Route::middleware("VerifyUserToken")->group(function () {
+
     //获取单个文章组成词语列表
     Route::prefix("get_list")->group(function () {
         Route::match(["post","get"],'Component', [ComponentController::class,"getList"]);
     });
-    //获取所有文章组成词语列表
-    Route::match(["post","get"],"get_component_list",[ArticleAssembleController::class,"getComponentList"]);
 
     //插入文章组成词语
     Route::prefix("insert")->group(function () {
         Route::match(["post","get"],'Component', [ComponentController::class,"insert"]);
+    });
+
+    //获取所有文章组成词语列表
+    Route::match(["post","get"],"get_component_list",[ArticleAssembleController::class,"getComponentList"]);
+
+    //用户操作
+    Route::prefix("user")->group(function () {
+        //新增
+        Route::match(["post","get"],"add_user",[UserController::class,"addUser"]);
+        //编辑
+        Route::match(["post","get"],"edit_user",[UserController::class,"editUser"]);
+        //删除
+        Route::match(["post","get"],"del_user",[UserController::class,"delUser"]);
     });
 });
 
